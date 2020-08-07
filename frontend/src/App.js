16599c4b781ce33c683236f367886a8225f3072a
s3678636 <s3678636@rmit.edu.vn>
2020-08-08 02:03:06 +0700

    Adding user login/regis feature built w aws incognito to frontend dev branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,78 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/js/dist/dropdown";
-import axios from 'axios';
-import logo from './logo.svg';
-import './assets/img/bg-masthead.jpg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import "./css/styles.css";
-import Home from "./Home.js";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import ProductAdmin from './components/ProductAdmin';
+import LogIn from './components/auth/LogIn';
+import Register from './components/auth/Register';
+import ForgotPassword from './components/auth/ForgotPassword';
+import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
+import ChangePassword from './components/auth/ChangePassword';
+import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
+import Welcome from './components/auth/Welcome';
+import Footer from './components/Footer';
+import { Auth } from 'aws-amplify';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+library.add(faEdit);
 
-const url = 'http://localhost:8080/serviceItems'
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      import: [],
-      sample_data: {}
-    };
-    this.getData = this.getData.bind(this);
-  }
-  useScript = (url, script) => {
-    // const script = document.createElement("script");
-
-    script.src = url;
-    script.async = true;
-
-    document.body.appendChild(script);
-  };
-
-  getData(ev){
-    axios.get(url)
-      .then(function(response) {
-        console.log(response.data)
-        ev.setState({ import: response.data }, ()=>{console.log(ev.state.import[0]) })
-        ev.setState({ sample_data: response.data[0] }, ()=>{console.log(ev.state.sample_data) })
-      });
+class App extends Component {
+  state = {
+    isAuthenticated: false,
+    isAuthenticating: true,
+    user: null
   }
 
-  componentDidMount() {
-    const script = document.createElement("script");
-    this.useScript("./js/scripts.js", script);
-    this.getData(this);
+  setAuthStatus = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  setUser = user => {
+    this.setState({ user: user });
+  }
+
+  async componentDidMount() {
+    try {
+      const session = await Auth.currentSession();
+      this.setAuthStatus(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      this.setUser(user);
+    }catch(error) {
+      console.log(error);
+    }
+    this.setState({ isAuthenticating: false });
   }
 
   render() {
+    const authProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      user: this.state.user,
+      setAuthStatus: this.setAuthStatus,
+      setUser: this.setUser
+    }
     return (
       <div className="App">
-        <Home serviceName={typeof this.state.sample_data !== undefined ? this.state.sample_data.service_name : null}/>
+        <Router>
+          <div>
+            <Navbar auth={authProps} />
+            <Switch>
+              <Route exact path="/" render={(props) => <Home {...props} auth={authProps} />} />
+              <Route exact path="/products" render={(props) => <Products {...props} auth={authProps} />} />
+              <Route exact path="/admin" render={(props) => <ProductAdmin {...props} auth={authProps} />} />
+              <Route exact path="/login" render={(props) => <LogIn {...props} auth={authProps} />} />
+              <Route exact path="/register" render={(props) => <Register {...props} auth={authProps} />} />
+              <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} />} />
+              <Route exact path="/forgotpasswordverification" render={(props) => <ForgotPasswordVerification {...props} auth={authProps} />} />
+              <Route exact path="/changepassword" render={(props) => <ChangePassword {...props} auth={authProps} />} />
+              <Route exact path="/changepasswordconfirm" render={(props) => <ChangePasswordConfirm {...props} auth={authProps} />} />
+              <Route exact path="/welcome" render={(props) => <Welcome {...props} auth={authProps} />} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
       </div>
     );
   }
